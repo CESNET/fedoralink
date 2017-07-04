f5c3e6e2f671393cf9ae188dbb620adbb93dfadb
Mirek Bauer <bauer@cesnet.cz>
2017-07-04 10:01:42 +0200

    Added test for GROUP BY implementation

---
+++ fedoralink/tests/test_query_aggregations.py
@@ -0,0 +1,28 @@
+import logging
+import unittest.util
+
+from fedoralink.tests.testserver.testapp.models import Simple
+from fedoralink.tests.utils import FedoraTestBase
+
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger('elasticsearch.trace').propagate = True
+
+unittest.util._MAX_LENGTH=2000
+
+
+class TestQueryAggregations(FedoraTestBase):
+    """
+    Test aggregation methods in queries 
+    """
+
+    def test_count_objects(self):
+        Simple.objects.create(text='Hello')
+        Simple.objects.create(text='Hello')
+        Simple.objects.create(text='Hello')
+        Simple.objects.create(text='World')
+
+        cnt = Simple.objects.filter(text='Hello').count()
+        self.assertEqual(3, cnt)
+        cnt = Simple.objects.count()
+        self.assertEqual(4, cnt)
+
